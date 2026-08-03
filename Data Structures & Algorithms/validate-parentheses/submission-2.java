c8ec97c056d81ce555d2cd50d44943c25a6c0921
Jin Bai <72070041+JinBa1@users.noreply.github.com>
2026-08-03 14:10:39 +0100

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public boolean isOpen(String a) {
+        Boolean isOpen = switch(a){
+            case "(" -> true;
+            case "{" -> true;
+            case "[" -> true;
+            default -> false;
+        };
+
+        return isOpen;
+    }
+
+
+    public boolean isValid(String s) {
+        var stack = new Stack<String>();
+
+        for(int i = 0; i < s.length(); i++) {
+            String curr = String.valueOf(s.charAt(i));
+
+            if (isOpen(curr)) {
+                stack.push(curr);
+            } else {
+                if (stack.isEmpty()) return false;
+                String poped = stack.pop();
+                if (!isOpen(poped)) return false;
+                Boolean isLegal = switch(poped + curr) {
+                    case "()" -> true;
+                    case "[]" -> true;
+                    case "{}" -> true;
+                    default -> false;
+                };
+
+                if (!isLegal) return false;
+            }
+
+        }
+
+        return stack.isEmpty();
+
+    }
+}
